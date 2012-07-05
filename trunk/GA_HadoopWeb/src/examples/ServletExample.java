9975e01e54ca01edc27b715152484e370d3be3e4
noisulli84@gmail.com <noisulli84@gmail.com@d7803bd0-05f9-b85a-d633-f4248bd4a642>
2012-07-05 15:06:59 +0000

    initial draft

---
+++ trunk/GA_HadoopWeb/src/examples/ServletExample.java
@@ -0,0 +1,41 @@
+package examples;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletExample
+ */
+public class ServletExample extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletExample() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String name = request.getParameter("userName");
+		String email = request.getParameter("email");
+		
+		response.sendRedirect("result.jsp?username=" + name + "&&email=" + email);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
