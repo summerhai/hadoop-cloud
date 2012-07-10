c3544545a2f68bd4f04808d4db158562ba07e8b2
dntuanvu <dntuanvu@d7803bd0-05f9-b85a-d633-f4248bd4a642>
2012-07-10 17:14:08 +0000

    file download dialog box

--- trunk/AWSSampleWeb/src/com/iss/cloud/team15/servletLayer/ResServlet.java
+++ trunk/AWSSampleWeb/src/com/iss/cloud/team15/servletLayer/ResServlet.java
@@ -12,7 +12,7 @@ import com.amazonaws.auth.*;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.GetObjectRequest;
 import com.amazonaws.services.s3.model.S3Object;
-import com.iss.cloud.team15.logicLayer.ElasticMapReduceApp;
+//import com.iss.cloud.team15.logicLayer.ElasticMapReduceApp;
 import com.iss.cloud.team15.util.Configuration;
 
 /**
@@ -34,20 +34,43 @@ public class ResServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		try {
+		/*try {
 			Thread elasticThread = new Thread(new ElasticMapReduceApp(),"MapReduce");
 			elasticThread.run();
 		} catch (Exception e) {
 			e.printStackTrace();
-		}
+		}*/
+	
 		
-		String key = getKey();
-		AWSCredentials myCredentials = new BasicAWSCredentials(key, getSecret()); 
-		AmazonS3Client s3Client = new AmazonS3Client(myCredentials);        
-		S3Object object = s3Client.getObject(new GetObjectRequest("iss.wordcount", key));
+		AWSCredentials myCredentials = new BasicAWSCredentials(getKey(), getSecret()); 
+		AmazonS3Client s3Client = new AmazonS3Client(myCredentials);   
+		String s3file = "output/Step1341934758692/part-r-00000";
+		S3Object object = s3Client.getObject(new GetObjectRequest("iss.wordcount", s3file));
 
 		BufferedReader reader = new BufferedReader(new InputStreamReader(object.getObjectContent()));
-		File file = new File("localFilename");      
+		File file = new File("D://localFilename");
+		
+		// Ram's example (start)
+		OutputStream myOut = response.getOutputStream( );
+		response.setContentType("text/plain");	     
+	    response.addHeader("Content-Disposition","attachment; filename=localFilename");	
+	    response.setContentLength( (int) file.length( ) );
+	    
+	    FileInputStream input = new FileInputStream(file);
+	    BufferedInputStream buf = new BufferedInputStream(input);
+	    
+	    int readBytes = 0;
+		while((readBytes = buf.read( )) != -1){
+			myOut.write(readBytes); 
+		}
+		if (myOut != null){
+			myOut.close( );
+		}   
+		if (buf != null){
+			buf.close( );
+		} 
+	    // Ram's Example (end)
+		
 		Writer writer = new OutputStreamWriter(new FileOutputStream(file));
 
 		while (true) {          
