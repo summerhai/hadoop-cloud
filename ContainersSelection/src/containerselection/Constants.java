98a1479a5f5c677dc8d8de5068ee728b3524dea3
noisulli84@gmail.com <noisulli84@gmail.com@d7803bd0-05f9-b85a-d633-f4248bd4a642>
2012-07-11 17:05:58 +0000

    amazon stuff

--- ContainersSelection/src/containerselection/Constants.java
+++ ContainersSelection/src/containerselection/Constants.java
@@ -1,7 +1,7 @@
 package containerselection;
 
 public final class Constants {
-	
+	/*
 	public static final String JOB1_INPUTFILE = "SeqInput";
 	public static final String JOB1_OUTPUTFILE = "output";
 	
@@ -12,12 +12,12 @@ public final class Constants {
 	public static final String SEQ_READ_FILE = "output/part-r-00000";
 	
 	public static final String CSV_FILE = "input/containersel.csv";
-	
+	*/
 	
 	/**
 	 * For Amazon purposes
 	 * 
-	
+	*/
 	public static final String JOB1_INPUTFILE = "s3n://iss.hadoop/seqInput";
 	public static final String JOB1_OUTPUTFILE = "s3n://iss.hadoop/output/job1_output";
 	
@@ -28,5 +28,5 @@ public final class Constants {
 	public static final String SEQ_READ_FILE = "s3n://iss.hadoop/output/job1_output/part-r-00000";
 	
 	public static final String CSV_FILE = "s3n://iss.hadoop/input/containersel.csv";
-	*/
+	
 }
